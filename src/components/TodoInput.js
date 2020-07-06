d07286a9439a2a307f1269fde67777537e6fbfa3
Lawrence <locaulker@gmail.com>
2020-07-06 08:16:24 -0400

    L69: handle functions started

--- src/components/TodoInput.js
+++ src/components/TodoInput.js
@@ -2,7 +2,34 @@ import React, { Component } from "react"
 
 class TodoInput extends Component {
   render() {
-    return <h1>hello from TodoInput</h1>
+    const { item, handleChange, handleSubmit, editItem } = this.props
+
+    return (
+      <div className="card card-body my-3">
+        <form onSubmit={handleSubmit}>
+          <div className="input-group">
+            <div className="input-group-prepend">
+              <div className="input-group-text bg-primary text-white">
+                <i className="fas fa-book"></i>
+              </div>
+            </div>
+            <input
+              type="text"
+              className="form-control text-capitalize"
+              placeholder="add todo item"
+              value={item}
+              onChange={handleChange}
+            />
+          </div>
+          <button
+            type="submit"
+            className="btn btn-block btn-primary mt-3 text-uppercase"
+          >
+            add to cart
+          </button>
+        </form>
+      </div>
+    )
   }
 }
 
